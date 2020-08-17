6106e858d8b813383f7b1518d8de22ba6b5453f4
francisuche <francisuche111@gmail.com>
2020-08-17 16:26:17 +0100

    3 files modified

--- src/app/basic-form.component.ts
+++ src/app/basic-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit }  from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+//formGroup is the class we use to tie different form controls into one group
+//formBuilder makes it easier to build forms
 
 @Component({
   selector: 'basic-form',
@@ -7,11 +10,21 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./basic-form.component.css']
 })
 
-export class BasicFormComponent{
-  constructor(){
+export class BasicFormComponent implements OnInit{
+
+  myForm : FormGroup;
+  constructor(private fb: FormBuilder){
   };
 
   ngOnInit(){
+
+    this.myForm = this.fb.group({  //schema that defines the validation stauts and fields in your form
+      email: '',
+      message: 'default message',
+      career: ''
+    })
+
+    this.myForm.valueChanges.subscribe(console.log)
 
   }
 
